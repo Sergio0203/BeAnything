45338576269bb872f7c2e218cbd3377535be2940
Sergio0203 <sergiocesar0203@gmail.com>
2024-05-13 16:07:49 -0400

    Initial Commit

---
+++ Challenge3/Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge3App.swift
+//  Challenge3
+//
+//  Created by Sérgio César Lira Júnior on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge3
+//
+//  Created by Sérgio César Lira Júnior on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
