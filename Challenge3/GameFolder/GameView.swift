4d980dd70e132bcd63c099eedd9aea32f4fd4cd3
Sergio <sergiocesar0203@gmail.com>
2024-05-16 13:46:56 -0400

    Drags added and orientation defined

--- Challenge3/GameFolder/GameView.swift
+++ Challenge3/GameFolder/GameView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct GameView: View {
-    let jobs = DataBase.listJobs
+    let jobs = DataBase.listJobs    
     
     var body: some View {
         GeometryReader { screen in
@@ -49,19 +49,30 @@ struct GameView: View {
                                     size: 28,
                                     relativeTo: .largeTitle))
                             
-                        }
-                        
-                        .frame(width: screen.size.width/3, height:screen.size.height/1.5, alignment: .top
-                        )
-                        .border(.black)
-                    }
-                    .frame(alignment: .center)
+                        }.frame(width: screen.size.width/3, height:screen.size.height/1.5, alignment: .top)
+                            .border(.black)
+                    }.frame(alignment: .center)
+                    
                     Spacer()
                 }
                 .frame(width: screen.size.width, height:screen.size.height, alignment: .top)
-            }
+                
+                
+            }.background(Color.white)
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                    .onEnded { value in
+                        let horizontalAmount = value.translation.width
+                        let verticalAmount = value.translation.height
+                        
+                        if abs(horizontalAmount) > abs(verticalAmount) {
+                            print(horizontalAmount < 0 ? "left swipe" : "right swipe")
+                        } else {
+                            print(verticalAmount < 0 ? "up swipe" : "down swipe")
+                        }
+                    })
         }
         .ignoresSafeArea()
+        
     }
 }
 
